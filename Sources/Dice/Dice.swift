7a2904225f87ea33960f22b7dda9be8deeac2087
Phil Hayes <phil@thinkhappymedium.com>
2019-10-07 08:42:58 -0500

    Init

--- Sources/Dice/Dice.swift
+++ Sources/Dice/Dice.swift
@@ -1,3 +1,18 @@
-struct Dice {
-    var text = "Hello, World!"
+import GameplayKit
+
+public struct Dice {
+	static let randomizer:GKRandom = GKARC4RandomSource()
+	var sides:Int = 6
+	
+	
+	public init(sides:Int) {
+		self.sides = sides
+	}
+	public init() {
+		
+	}
+	public func roll() -> Int {
+		Dice.randomizer.nextInt(upperBound: sides) + 1
+	}
+	
 }
--- Tests/DiceTests/DiceTests.swift
+++ Tests/DiceTests/DiceTests.swift
@@ -2,14 +2,26 @@ import XCTest
 @testable import Dice
 
 final class DiceTests: XCTestCase {
-    func testExample() {
+	func testRollDice() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Dice().text, "Hello, World!")
-    }
+		
+		let dSixes = [6,6,6,6,6,6,6,6].map {Dice(sides: $0)}
+			.map{$0.roll()}
+		print(dSixes)
+		
+		let dTwenties = [20,20,20,20,20,20,20].map {Dice(sides: $0)}
+			.map{$0.roll()}
+		print(dTwenties)
+		
+		let mixedDie =  [20, 8,8,4, 20, 8, 8, 4].map {Dice(sides: $0)}
+			.map{($0.sides, $0.roll())}
+		print(mixedDie)
+		
+	}
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testRollDice", testRollDice),
     ]
 }
